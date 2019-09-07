1849af01310a993c62f306c4154d46f194c315d7
Sam Carlton <sam@sam.lc>
2019-09-07 13:29:19 -0500

    Add Interstate font

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,14 @@ import '../styles/styles.css'
 import React from 'react'
 import App from 'next/app'
 
+import Fonts from '../lib/fonts'
+
 export default class Should extends App {
+
+  componentDidMount = () => {
+    Fonts()
+  }
+
   render () {
     const { Component, pageProps } = this.props
 
